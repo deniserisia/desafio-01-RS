4f3570cdb8221692daf928ae46ebf85555844f98
Denise Risia <risiaresponde@gmail.com>
2023-06-24 16:42:52 -0300

    Add da section home

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 // toggle icon navbar
-//let menuIcon = document.querySelector('#menu-icon');
-//let navbar = document.querySelector('.navbar');
-
-//menuIcon.onclick = () =>
-//{
-//    menuIcon.classList.toggle('bx-x');
-//    navbar.classList.toggle('active');
-//}
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-if (menuIcon && navbar) {
-  menuIcon.onclick = () => {
+menuIcon.onclick = () =>
+{
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-  }
 }
-
-
 
 // scroll sections
 let sections = document.querySelectorAll('section');
@@ -44,4 +32,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // rm
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
